8d023a8029b187c64ef356e94b5441359659098e
numb3r3 <wangfelix87@gmail.com>
2023-04-04 15:36:36 +0800

    init commit

---
+++ opengpt/hub.py
@@ -0,0 +1,11 @@
+"""The model hub is a central repository of models that can be used to load pretrained models."""
+
+
+def load_model_from_hub(model_name: str, **kwargs):
+    """Load a model from the model hub.
+
+    :param model_name: The name of the model to load.
+    :param kwargs: Additional arguments to pass to the model.
+    :return: The model.
+    """
+    ...
