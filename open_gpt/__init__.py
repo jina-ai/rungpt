6cf954fe967a7f4109240eab7804c04fdb613231
numb3r3 <wangfelix87@gmail.com>
2023-04-27 14:07:06 +0800

    fix: cli

--- open_gpt/__init__.py
+++ open_gpt/__init__.py
@@ -1,7 +1,34 @@
+import os as _os
 import sys
+import warnings as _warnings
+
+import jina as _jina
+
+
+def _warning_on_one_line(message, category, filename, lineno, *args, **kwargs):
+    return '\033[1;33m%s: %s\033[0m \033[1;30m(raised from %s:%s)\033[0m\n' % (
+        category.__name__,
+        message,
+        filename,
+        lineno,
+    )
+
+
+_warnings.formatwarning = _warning_on_one_line
+_warnings.simplefilter('ignore', category=DeprecationWarning)
+
+# hide jina user survey
+_os.environ['JINA_HIDE_SURVEY'] = '1'
+
+
+try:
+    __jina_version__ = _jina.__version__
+except AttributeError as e:
+    raise RuntimeError(
+        '`jina` dependency is not installed correctly, please reinstall with `pip install -U --force-reinstall jina`'
+    )
 
 # import importlib.metadata if available, otherwise importlib_metadata
-# (for Python < 3.8)
 if sys.version_info >= (3, 8):
     import importlib.metadata as importlib_metadata
 else:
--- open_gpt/cli/application.py
+++ open_gpt/cli/application.py
@@ -21,6 +21,7 @@ def load_command(name: str) -> Callable[[], Command]:
 
 COMMANDS = [
     "about",
+    "serve",
 ]
 
 
