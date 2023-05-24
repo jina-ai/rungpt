fix: precision auto cast

open_gpt/helper.py
@@ -10,10 +10,9 @@ import torch.distributed as dist
 _PRECISION_TO_DTYPE = {
     'fp16': torch.float16,
     'fp32': torch.float32,
-    'int8': torch.int8,
-    'torch.float32': torch.float32,
-    'torch.float16': torch.float16,
-    'torch.int8': torch.int8,
+    'int8': torch.float16,
+    'float32': torch.float32,
+    'float16': torch.float16,
 }
 
 _DEFAULT_DTYPE = torch.float32
@@ -22,14 +21,14 @@ _DEFAULT_DTYPE = torch.float32
 def cast_torch_dtype(precision: Union[str, 'torch.dtype']):
     assert precision is not None
     if isinstance(precision, str):
-        return _PRECISION_TO_DTYPE.get(precision, precision)
+        return _PRECISION_TO_DTYPE.get(precision)
     elif isinstance(precision, torch.dtype):
         return precision
     else:
         return ValueError(f'Invalid precision: {precision}')
 
 
-def cast_precision(dtype: Optional[Union[str, 'torch.dtype']]):
+def cast_to_precision(dtype: Optional[Union[str, 'torch.dtype']]) -> str:
     if isinstance(dtype, str):
         return dtype
     elif dtype == torch.float32:

open_gpt/models/loading.py
@@ -1,16 +1,18 @@
 from typing import List, Optional, Union
 
 import torch
-from loguru import logger
 
 from open_gpt.helper import auto_dtype_and_device
+
+from ..logging import logger
 
 
 def load_model_and_tokenizer(
     model_name_or_path: str,
     tokenizer_name_or_path: Optional[str] = None,
     device: Optional[torch.device] = None,
-    dtype: Optional[Union[str, torch.dtype]] = None,
+    precision: Optional[str] = None,
+    dtype: Optional[torch.dtype] = None,
     device_map: Optional[Union[str, List[int]]] = None,
     no_split_module_classes: Optional[List[str]] = None,
     **kwargs,
@@ -20,17 +22,14 @@ def load_model_and_tokenizer(
 
     from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 
-    dtype, device = auto_dtype_and_device(dtype, device)
-
     tokenizer = AutoTokenizer.from_pretrained(
         tokenizer_name_or_path or model_name_or_path, trust_remote_code=True
     )
 
     if tokenizer.pad_token is None:
         # Issue: GPT models don't have a pad token
-        # tokenizer.add_special_tokens({"pad_token": "<PAD>"})
-        tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.pad_token_id = tokenizer.eos_token_id
+        tokenizer.pad_token = tokenizer.unk_token
+        tokenizer.pad_token_id = tokenizer.unk_token_id
 
     # For generation padding tokens should be on the left
     tokenizer.padding_side = "left"
@@ -52,21 +51,34 @@ def load_model_and_tokenizer(
             # make sure token embedding weights are still tied if needed
             model.tie_weights()
 
-            model = load_checkpoint_and_dispatch(
-                model,
-                model_path,
-                device_map=device_map,
-                no_split_module_classes=no_split_module_classes,
-                dtype=dtype,
+        if precision == 'bit8':
+            from transformers.utils.bitsandbytes import replace_8bit_linear
+
+            model = replace_8bit_linear(
+                model, threshold=6.0, modules_to_not_convert=["lm_head"]
             )
+
+        model = load_checkpoint_and_dispatch(
+            model,
+            model_path,
+            device_map=device_map,
+            no_split_module_classes=no_split_module_classes,
+            dtype=dtype if precision != 'bit8' else None,
+        )
     else:
+        logger.warning(
+            f'To turn on tensor parallelism, set `device_map` to a list of GPU ids rather than `None`'
+        )
         model = AutoModelForCausalLM.from_pretrained(
-            model_name_or_path, torch_dtype=dtype, trust_remote_code=True
+            model_name_or_path,
+            torch_dtype=dtype,
+            load_in_8bit=precision == 'bit8',
+            trust_remote_code=True,
         )
         model.to(device)
 
     if hasattr(model, 'generation_config'):
-        # set pad_token_id to eos_token_id because GPT does not have a PAD token
-        model.generation_config.pad_token_id = model.generation_config.eos_token_id
+        # set pad_token_id to unk_token_id because GPT does not have a PAD token
+        model.generation_config.pad_token_id = model.generation_config.unk_token_id
 
     return model, tokenizer

open_gpt/models/modeling.py
@@ -4,7 +4,6 @@ import torch
 from torch import nn
 
 from ..helper import auto_dtype_and_device
-from ..logging import logger
 from .generation import GenerationMixin
 
 if TYPE_CHECKING:
@@ -20,7 +19,7 @@ class BaseModel(nn.Module, GenerationMixin):
         self,
         model_name_or_path: str,
         tokenizer_name_or_path: Optional[str] = None,
-        dtype: Optional[Union[str, torch.dtype]] = None,
+        precision: str = 'fp32',
         device: Optional[torch.device] = None,
         device_map: Optional[Union[str, List[int]]] = None,
         eval_mode: bool = True,
@@ -32,13 +31,10 @@ class BaseModel(nn.Module, GenerationMixin):
 
         self._model_name_or_path = model_name_or_path
 
-        self._dtype, self._device = auto_dtype_and_device(dtype, device)
+        self._precision = precision
+        self._dtype, self._device = auto_dtype_and_device(precision, device)
 
         self._device_map = device_map
-        if not self._device_map:
-            logger.warning(
-                f'To turn on tensor parallelism, set `device_map` to a list of GPU ids rather than `None`'
-            )
 
         self._eval_mode = eval_mode
 
@@ -56,6 +52,7 @@ class BaseModel(nn.Module, GenerationMixin):
         self.model, self.tokenizer = load_model_and_tokenizer(
             model_name_or_path,
             tokenizer_name_or_path=tokenizer_name_or_path,
+            precision=self._precision,
             dtype=self._dtype,
             device=self._device,
             device_map=self._device_map,

open_gpt/models/vicuna/loading.py
@@ -11,7 +11,8 @@ def load_model_and_tokenizer(
     model_name_or_path: str,
     tokenizer_name_or_path: Optional[str] = None,
     device: Optional[torch.device] = None,
-    dtype: Optional[Union[str, torch.dtype]] = None,
+    precision: Optional[str] = None,
+    dtype: Optional[torch.dtype] = None,
     device_map: Optional[Union[str, List[int]]] = None,
     no_split_module_classes: Optional[List[str]] = None,
     **kwargs,
@@ -38,8 +39,8 @@ def load_model_and_tokenizer(
     if tokenizer.pad_token is None:
         # Issue: GPT models don't have a pad token
         # tokenizer.add_special_tokens({"pad_token": "<PAD>"})
-        tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.pad_token_id = tokenizer.eos_token_id
+        tokenizer.pad_token = tokenizer.unk_token
+        tokenizer.pad_token_id = tokenizer.unk_token_id
 
     # For generation padding tokens should be on the left
     tokenizer.padding_side = "left"
@@ -81,7 +82,7 @@ def load_model_and_tokenizer(
 
     logger.info(f"Loading model weights delta from {model_name_or_path}")
     delta = AutoModelForCausalLM.from_pretrained(
-        model_name_or_path, torch_dtype=torch.float16, low_cpu_mem_usage=True
+        model_name_or_path, torch_dtype=dtype, low_cpu_mem_usage=True
     )
     # adapted from `fastchat.model.apply_delta`
     for name, param in tqdm(model.state_dict().items(), desc="Applying delta"):
@@ -91,5 +92,13 @@ def load_model_and_tokenizer(
     # clean up delta to save memory
     delta = None
     torch.cuda.empty_cache()
+
+    # TODO: better way to do this?
+    if precision == 'bit8':
+        from transformers.utils.bitsandbytes import replace_8bit_linear
+
+        model = replace_8bit_linear(
+            model, threshold=6.0, modules_to_not_convert=["lm_head"]
+        )
 
     return model, tokenizer