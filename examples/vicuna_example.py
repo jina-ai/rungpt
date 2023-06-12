032d7a1b68ebde1f6b8504e98630fcb540e512b2
numb3r3 <wangfelix87@gmail.com>
2023-06-12 17:28:02 +0800

    fix: lora adapter example

--- examples/vicuna_example.py
+++ examples/vicuna_example.py
@@ -2,11 +2,17 @@ import open_gpt
 from open_gpt.profile import end_measure, log_measures, start_measure
 
 start_measures = start_measure()
+# model = open_gpt.create_model(
+#     'lmsys/vicuna-7b-delta-v1.1', precision='fp16', device_map='balanced'
+# )
 model = open_gpt.create_model(
-    'lmsys/vicuna-7b-delta-v1.1', precision='fp16', device_map='balanced'
+    'CarperAI/stable-vicuna-13b-delta', precision='fp16', device_map='balanced'
 )
 
-prompt = 'The goal of life is'
+prompt = """\
+### Human: Write a Python script for text classification using Transformers and PyTorch
+### Assistant:\
+"""
 
 generated_text = model.generate(
     prompt,
@@ -18,4 +24,4 @@ generated_text = model.generate(
 )
 print(f'==> {prompt} {generated_text}')
 end_measures = end_measure(start_measures)
-log_measures(end_measures, "Model generation (single)")
+log_measures(end_measures, "Model generation")
