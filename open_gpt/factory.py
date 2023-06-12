442dce0934e21784b7064b21f669b1ebf0ede7c1
numb3r3 <wangfelix87@gmail.com>
2023-06-12 15:54:13 +0800

    fix: moss example

--- open_gpt/factory.py
+++ open_gpt/factory.py
@@ -6,7 +6,7 @@ import torch
 
 def create_model(
     model_name: str,
-    precision: str = 'fp32',
+    precision: str = 'fp16',
     adapter_name_or_path: Optional[str] = None,
     device: Optional[Union[str, torch.device]] = None,
     device_map: Optional[Union[str, List[int]]] = None,
@@ -15,8 +15,9 @@ def create_model(
     """Create a model.
 
     :param model_name: The name of the model to create.
-    :param adapter_name_or_path: The name of adapter model used.
-    :param precision: The precision to use for the model. Can be one of ``"fp16"``, ``"fp32"`` or ``"int8"``. Defaults to ``"fp32"``.
+    :param adapter_name_or_path: The name or path of the adapter to use for the model.
+            This is only used for models that support adapters for fine-tuning. Defaults to ``None``.
+    :param precision: The precision to use for the model. Can be one of ``"fp16"``, ``"fp32"``, ``"bit8"`` or ``"bit4"``. Defaults to ``"fp16"``.
     :param device: The device to use. Can be one of ``"cpu"``, ``"cuda"``, ``"cuda:X"`` or ``None``.
     :param device_map: The device map to use. Can be one of ``"balanced"``, ``"single"`` or a list of device IDs.
     :param kwargs: Additional keyword arguments to pass to the model.
@@ -35,7 +36,9 @@ def create_model(
             device_map=device_map,
             **kwargs,
         )
-    elif model_name.startswith('lmsys/vicuna'):
+    elif model_name.startswith('lmsys/vicuna') or model_name.startswith(
+        'CarperAI/stable-vicuna'
+    ):
         assert not model_name.endswith(
             'v0'
         ), 'You are using an outdated model, please use the newer version ``v1.1+``'
@@ -62,9 +65,7 @@ def create_model(
             device_map=device_map,
             **kwargs,
         )
-    elif model_name.startswith('stabilityai/stablelm') or model_name.startswith(
-        'CarperAI/stable-vicuna'
-    ):
+    elif model_name.startswith('stabilityai/stablelm'):
         from .models.stablelm.modeling import StableLMModel
 
         assert adapter_name_or_path is None, 'StableLM does not support adapter'
