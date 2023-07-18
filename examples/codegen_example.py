012064cecc8ffc0f43bd46476f049e4dcfc6322d
jemmyshin <jem.fu@jina.ai>
2023-07-18 17:13:51 +0800

    feat: add codegen

---
+++ examples/codegen_example.py
@@ -0,0 +1,17 @@
+import open_gpt
+from open_gpt.profile import end_measure, log_measures, start_measure
+
+start_measures = start_measure()
+model = open_gpt.create_model(
+    'Salesforce/codegen-350M-mono', precision='fp16', device_map='balanced'
+)
+
+prompt = 'def hello_world():'
+
+
+generated_text = model.generate(
+    prompt, max_new_tokens=256, do_sample=False, temperature=0.9
+)
+print(f'==> {prompt} {generated_text}')
+end_measures = end_measure(start_measures)
+log_measures(end_measures, "Model generation")
